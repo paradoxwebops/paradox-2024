cae1795da88749f5a0026c577c34c2eb7f194dc9
Yuv <billiondollarone07@gmail.com>
2024-05-06 17:53:35 +0530

    event page error rectification

--- components/EventCard/index.tsx
+++ components/EventCard/index.tsx
@@ -11,7 +11,7 @@ export default function EventCard({ event }: { event: allEventData }) {
       className=" border-[#6140E2] text-black from-[#AD2FFF] flex flex-col to-[#230137] w-full rounded-xl p-4"
     >
       <div className="rounded-xl wrap container wrap--1 w-full h-full flex flex-col gap-2">
-        <Image
+        <img
           src={event.header_image}
           alt={event.name}
           className="w-full h-[300px] border-[#6140E2] box object-cover rounded-xl"
